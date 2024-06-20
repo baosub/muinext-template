0889c60504cfea03302f4c09bb65da51cb18e673
Baldo <barrosoub@protonmail.com>
2024-06-20 08:16:52 -0600

    all dependecies were installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import  Button from '@mui/material/Button'
 import React from 'react'
+
 
 const Home = () => {
   return (
     <>
+    <Button>
     This is Home
+
+    </Button>
+   
       
     </>
   )
